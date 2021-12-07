7aa79e4922aaeffbe915d6990a301e8a837e5f81
Jorge Salcedo <jorged94@gmail.com>
2021-12-07 17:36:03 -0500

    creo que esta bien xD

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,23 +13,31 @@ import org.springframework.stereotype.Component;
 public class BootStrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher alfaguara = new Publisher("Alfaguara", "Lima Peru");
+        publisherRepository.save(alfaguara);
         
         Author jkrowling = new Author("Johane", "Rowling");
         Book harrypotter = new Book("Harry Potter", "243242");
 
         jkrowling.getBooks().add(harrypotter);
         harrypotter.getAuthors().add(jkrowling);
+//        harrypotter.setPublisher(alfaguara);
 
-        bookRepository.save(harrypotter);
         authorRepository.save(jkrowling);
+        bookRepository.save(harrypotter);
+
 
 
         Author ggmarques = new Author("Gabriel", "Garcia");
@@ -35,12 +45,23 @@ public class BootStrapData implements CommandLineRunner {
 
         ggmarques.getBooks().add(cienanos);
         cienanos.getAuthors().add(ggmarques);
+        cienanos.setPublisher(alfaguara);
 
-        bookRepository.save(cienanos);
         authorRepository.save(ggmarques);
+        bookRepository.save(cienanos);
+
+        alfaguara.getBooks().add(harrypotter);
+        alfaguara.getBooks().add(cienanos);
+
+        publisherRepository.save(alfaguara);
+
 
         System.out.println("Pruebita uwu");
-        System.out.printf("Numero de libros: "+ bookRepository.count());
+        System.out.println("Numero de libros: "+ bookRepository.count());
+        System.out.println("Numero de publishers: "+ publisherRepository.count());
+
+        System.out.println("libros de alfaguara: " + alfaguara.getBooks().size());
+//        System.out.println(cienanos.toString());
 
 
     }
