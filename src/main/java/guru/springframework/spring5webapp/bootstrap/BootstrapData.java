58b4ae8830b3b2e32df01942a8346d875ab7710c
Jorge Salcedo <jorged94@gmail.com>
2021-12-07 01:51:15 -0500

    1st commit uwu

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,48 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        Author jkrowling = new Author("Johane", "Rowling");
+        Book harrypotter = new Book("Harry Potter", "243242");
+
+        jkrowling.getBooks().add(harrypotter);
+        harrypotter.getAuthors().add(jkrowling);
+
+        bookRepository.save(harrypotter);
+        authorRepository.save(jkrowling);
+
+
+        Author ggmarques = new Author("Gabriel", "Garcia");
+        Book cienanos = new Book("Cien Años", "243242");
+
+        ggmarques.getBooks().add(cienanos);
+        cienanos.getAuthors().add(ggmarques);
+
+        bookRepository.save(cienanos);
+        authorRepository.save(ggmarques);
+
+        System.out.println("Pruebita uwu");
+        System.out.printf("Numero de libros: "+ bookRepository.count());
+
+
+    }
+}
